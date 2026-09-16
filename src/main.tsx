721bdea29bdd46cfac97cb6ddd987af8e309b3be
Alex Frolov <fr.280907@gmail.com>
2026-09-16 21:15:13 +0200

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'modern-normalize';
-import './index.module.css';
+import './index.css';
 import App from './components/App.tsx';
 
 const queryClient = new QueryClient();
